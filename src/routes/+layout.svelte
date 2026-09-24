583763710fc00aa52178686939823c3ee483ae82
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-24 19:33:05 +0200

    l

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,57 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <header>
+    <nav>
+      <ul>
+        <li><a class="active" href="">Home</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Blabla</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <style>
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      gap: 30px;
+    }
+
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 6px 10px;
+      border-radius: 8px;
+
+      &:hover {
+        background: #a32d2d;
+      }
+    }
+
+    nav {
+      display: flex;
+      background: var(--black);
+      padding: 20px;
+    }
+
+    .active {
+      background: #a32d2d;
+    }
+
+    header {
+      justify-content: center;
+      display: flex;
+      align-items: center;
+    }
+  </style>
 </svelte:head>
 
 {@render children()}
